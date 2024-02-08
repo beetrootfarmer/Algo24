7a77e4d68748b91dac397284005cb44717568eec
beetrootfarmer <pos04118@gmail.com>
2024-02-08 23:57:29 +0900

    광물 idea

--- 240203_광물캐기.js
+++ 240203_광물캐기.js
@@ -1,6 +1,28 @@
 function solution(picks, minerals) {
-    var answer = 0;
-    // 보유하고 있는 곡갱이에 따라서 
-    // 피로도가 가장 적은 조합을 먼저 확인하고
+    var answer = 0; 
+    let index = 0;
+    // console.log(picks) [dia, iron, stone]
+    // console.log(minerals)
+    
+    // 1. 다이아몬드 곡괭이가 있을 경우 모두 처리
+    // index = (다이아몬드 곡괭이 갯수 * 5)
+    index += picks[0] * 5
+    
+    let left = minerals.length - (index+1)
+    if (left > 0){
+        // 2. 철 곡괭이가 있을 경우 처리
+        for (let i = index ; i < minerals.length; i++){
+            
+        }
+    } else{
+        answer = index
+    }
+    
+    
+    // 3. 남은 것들은 돌 곡괭이로 처리
+    
+    // 다이아몬드 > all / all > 돌 / 철 > 철
+    // 철 > 다이아몬드 , 돌 > 철
+    // 
     return answer;
 }
